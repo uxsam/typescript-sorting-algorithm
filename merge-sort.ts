58d83c07577664d0e613b20e84acda66ae529b60
uxsam <samm.mathai@gmail.com>
2020-06-11 21:48:41 -0400

    Adding Types
    This change will sort desc for both strings and number

--- merge-sort.ts
+++ merge-sort.ts
@@ -1,19 +1,22 @@
+type inputType = string[] | number[];
+
 
 class MergeSort {
-    private _sortedArray: (number|string)[] =[];
-    get sortedArray(): (number|string)[]{
+    private _sortedArray: inputType =[];
+    get sortedArray(): inputType{
         return this._sortedArray;
     }
     
-    constructor(arraySort: (number|string)[]) {
-        this._sortedArray = this.functionMergsort(arraySort);
+    constructor(arraySort: inputType ) {
+       this._sortedArray = this.functionMergsort(arraySort);
     }
 
-    private merge<T>(left: Array<T>, right: Array<T>) {
-        if (!left && !right)
+    private merge(left:inputType , right:inputType): inputType {
+        if (left.length < 0 && right.length < 0) { 
             return [];
-        let mainarray = left.concat(right);
-        mainarray.sort((a, b) => {
+        }
+        let mainarray = ([...left,...right] as inputType);
+        mainarray.sort((a: string|number , b : string|number) => {
             if (a > b) {
                 return 1
             }
@@ -25,16 +28,19 @@ class MergeSort {
         return mainarray;
     }
 
-    private functionMergsort<T>(arrayToSplit: Array<T>): Array<T> {
+    private functionMergsort (arrayToSplit:inputType): inputType  {
         let arraySize = arrayToSplit.length;
-        if (arraySize < 2) {
+        if (arraySize < 2 ) {
             return arrayToSplit;
         }
         let splitSize = Math.round(arraySize / 2);
          //split the main array to left and right
         let left = arrayToSplit.slice(0, splitSize);
         let right = arrayToSplit.slice(splitSize, arraySize);
-        return this.merge(this.functionMergsort(left), this.functionMergsort(right));
+        return this.merge(left,right);
     }
 }
+let input: inputType = ['janet j','janet a','sam m', 'jeena m', 'luis b','raman v'];
+console.log(new MergeSort(input).sortedArray);
+input = [1, 2, 6, 4,2,4,66,78,0,44,22,42,12];
 console.log(new MergeSort(input).sortedArray);
