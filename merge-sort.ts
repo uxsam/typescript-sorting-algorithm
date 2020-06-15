42660426a1f98bf9911bd2724d3b554e5210448c
uxsam <samm.mathai@gmail.com>
2020-06-15 07:55:46 -0400

    Update merge-sort.ts
    Adding Edge cases

--- merge-sort.ts
+++ merge-sort.ts
@@ -1,3 +1,4 @@
+
 type inputType = string[] | number[];
 
 
@@ -10,41 +11,63 @@ class MergeSort {
     
     constructor(unSortedArray: inputType ) {
         this._sortedArray = this.functionMergesort(unSortedArray);
-        console.log(this.opertionsCounter++);
+        console.log(`oprations count = ${this.opertionsCounter}`);
     }
 
     private merge(left:inputType , right:inputType): inputType {
         if (left.length < 0 && right.length < 0) { 
             return [];
         }
-        let mainarray :(string|number) =[];
+        let mainarray: any = [];
+        
         let rightStartCounter = 0;
-        for (let i=0; i < left.length; i++) {
+        let leftStartCounter = 0;
+        
+        for (let i = leftStartCounter ; i < left.length; i++) {
             for (let j = rightStartCounter; j < right.length; j++){
-
-                //[1, 2, 6, 4,2,4,  66,78,0,44,22,42,12];
-                if (left[i] <  right[j]) {
+                this.opertionsCounter++;
+               
+                if (left[i] < right[j]) {
                     mainarray.push(left[i]);
-                    //end of array
-                    if (i === left.length-1  ) {
+                    if (j +1  === right.length && i+1 === left.length) {
+                        mainarray.push(right[j]);
+                       //right  has items but left is empty 
+                    } else if (j + 1 < right.length && i + 1 === left.length) {
                         mainarray.push(...right.slice(j,right.length));
                     }
+                    
                     break;
-                } else if (left[i] >  right[j]) { 
+                    
+                } else if (left[i] > right[j]) { 
                     mainarray.push(right[j]);
                     rightStartCounter++;
-                    
-                     if(j === right.length - 1){
-                         mainarray.push(...left.slice(i, left.length));
+                    if (j+1  === right.length && i+1  === left.length) {
+                        mainarray.push(left[i]);
+                    }
+                    //left has items and right is empty  
+                    else if (j + 1 === right.length && i + 1 < left.length) {
+                        mainarray.push(...left.slice(i, left.length));
+                        i = left.length;
+                        break;
                     }
                     
-                }
-                else if (left[i] === right[j]) { 
+                } else if (left[i] === right[j]) { 
                     mainarray.push(left[i]);
                     mainarray.push(right[j]);
                     rightStartCounter++;
+                     if (j + 1 === right.length && i + 1 < left.length) {
+                        mainarray.push(...left.slice(i + 1, left.length));
+                        i=left.length;
+
+                    }
+                    else if (j + 1 < right.length && i + 1 === left.length) {
+                         mainarray.push(...right.slice(j + 1, right.length));
+                         j = right.length;
+                    }
                     break;
                 }
+                
+                
             }
             
         }
@@ -66,6 +89,12 @@ class MergeSort {
     }
 }
 
-let input = [1, 2, 6, -1, 0, 77, 64, 4, 4];
+let input = Array.from({ length: 2328 }, () => Math.round(Math.random()*93*13 * 100));
 
-console.log(new MergeSort(input).sortedArray);
+let finalArray = new MergeSort(input).sortedArray;
+
+// if (input.length !== finalArray.length) {
+//     console.log('fail');
+// } else {
+//     console.log('pass');
+// }
